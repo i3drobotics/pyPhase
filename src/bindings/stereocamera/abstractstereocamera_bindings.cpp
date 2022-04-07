f4ba1d634672557a75a2b11a4af7875b06abd6b5
Ben Knight <benknight135@gmail.com>
2022-04-07 19:22:11 +0100

    add abstract bindings & data capture tests

--- src/bindings/stereocamera/abstractstereocamera_bindings.cpp
+++ src/bindings/stereocamera/abstractstereocamera_bindings.cpp
@@ -18,14 +18,36 @@ namespace py = pybind11;
 void init_abstractstereocamera(py::module_ &m) {
     NDArrayConverter::init_numpy();
 
-    py::class_<I3DR::Phase::CameraReadResult>(m, "CameraReadResult")
-        .def(py::init<bool,cv::Mat,cv::Mat>())
-        .def_readwrite("valid", &I3DR::Phase::CameraReadResult::valid)
-        .def_readwrite("left_image", &I3DR::Phase::CameraReadResult::left_image)
-        .def_readwrite("right_image", &I3DR::Phase::CameraReadResult::right_image);
-
     py::class_<I3DR::Phase::AbstractStereoCamera>(m, "AbstractStereoCamera")
-        .def(py::init<I3DR::Phase::CameraDeviceInfo>());
+        .def("connect", &I3DR::Phase::AbstractStereoCamera::connect)
+        .def("isConnected", &I3DR::Phase::AbstractStereoCamera::isConnected)
+        .def("startCapture", &I3DR::Phase::AbstractStereoCamera::startCapture)
+        .def("stopCapture", &I3DR::Phase::AbstractStereoCamera::stopCapture)
+        .def("isCapturing", &I3DR::Phase::AbstractStereoCamera::isCapturing)
+        .def("getFrameRate", &I3DR::Phase::AbstractStereoCamera::getFrameRate)
+        .def("setExposure", &I3DR::Phase::AbstractStereoCamera::setExposure)
+        .def("enableHardwareTrigger", &I3DR::Phase::AbstractStereoCamera::enableHardwareTrigger)
+        .def("setFrameRate", &I3DR::Phase::AbstractStereoCamera::setFrameRate)
+        .def("setLeftAOI", &I3DR::Phase::AbstractStereoCamera::setLeftAOI)
+        .def("setRightAOI", &I3DR::Phase::AbstractStereoCamera::setRightAOI)
+        .def("read", &I3DR::Phase::AbstractStereoCamera::read, py::arg("timeout") = 1000)
+        .def("setTestImagePaths", &I3DR::Phase::AbstractStereoCamera::setTestImagePaths)
+        .def("startReadThread", &I3DR::Phase::AbstractStereoCamera::startReadThread, py::arg("timeout") = 1000)
+        .def("isReadThreadRunning", &I3DR::Phase::AbstractStereoCamera::isReadThreadRunning)
+        .def("getReadThreadResult", &I3DR::Phase::AbstractStereoCamera::getReadThreadResult)
+        .def("getWidth", &I3DR::Phase::AbstractStereoCamera::getWidth)
+        .def("getHeight", &I3DR::Phase::AbstractStereoCamera::getHeight)
+        .def("getDownsampleFactor", &I3DR::Phase::AbstractStereoCamera::getDownsampleFactor)
+        .def("enableDataCapture", &I3DR::Phase::AbstractStereoCamera::enableDataCapture)
+        .def("setDataCapturePath", &I3DR::Phase::AbstractStereoCamera::setDataCapturePath)
+        .def("getCaptureCount", &I3DR::Phase::AbstractStereoCamera::getCaptureCount)
+        .def("resetCaptureCount", &I3DR::Phase::AbstractStereoCamera::resetCaptureCount)
+        .def("setLeftFlipX", &I3DR::Phase::AbstractStereoCamera::setLeftFlipX)
+        .def("setLeftFlipY", &I3DR::Phase::AbstractStereoCamera::setLeftFlipY)
+        .def("setRightFlipX", &I3DR::Phase::AbstractStereoCamera::setRightFlipX)
+        .def("setRightFlipY", &I3DR::Phase::AbstractStereoCamera::setRightFlipY)
+        .def("setDownsampleFactor", &I3DR::Phase::AbstractStereoCamera::setDownsampleFactor)
+        .def("disconnect", &I3DR::Phase::AbstractStereoCamera::disconnect);
 
     //TODO add callback funtion for read results (https://pybind11.readthedocs.io/en/stable/advanced/cast/functional.html)
 
--- src/bindings/stereocamera/phobosstereocamera_bindings.cpp
+++ src/bindings/stereocamera/phobosstereocamera_bindings.cpp
@@ -19,6 +19,35 @@ void init_phobosstereocamera(py::module_ &m) {
     NDArrayConverter::init_numpy();
 
     py::class_<I3DR::Phase::PhobosStereoCamera>(m, "PhobosStereoCamera")
-        .def(py::init<I3DR::Phase::CameraDeviceInfo>());
+        .def(py::init<I3DR::Phase::CameraDeviceInfo>())
+        .def("connect", &I3DR::Phase::PhobosStereoCamera::connect)
+        .def("isConnected", &I3DR::Phase::PhobosStereoCamera::isConnected)
+        .def("startCapture", &I3DR::Phase::PhobosStereoCamera::startCapture)
+        .def("stopCapture", &I3DR::Phase::PhobosStereoCamera::stopCapture)
+        .def("isCapturing", &I3DR::Phase::PhobosStereoCamera::isCapturing)
+        .def("getFrameRate", &I3DR::Phase::PhobosStereoCamera::getFrameRate)
+        .def("setExposure", &I3DR::Phase::PhobosStereoCamera::setExposure)
+        .def("enableHardwareTrigger", &I3DR::Phase::PhobosStereoCamera::enableHardwareTrigger)
+        .def("setFrameRate", &I3DR::Phase::PhobosStereoCamera::setFrameRate)
+        .def("setLeftAOI", &I3DR::Phase::PhobosStereoCamera::setLeftAOI)
+        .def("setRightAOI", &I3DR::Phase::PhobosStereoCamera::setRightAOI)
+        .def("read", &I3DR::Phase::PhobosStereoCamera::read, py::arg("timeout") = 1000)
+        .def("setTestImagePaths", &I3DR::Phase::PhobosStereoCamera::setTestImagePaths)
+        .def("startReadThread", &I3DR::Phase::PhobosStereoCamera::startReadThread, py::arg("timeout") = 1000)
+        .def("isReadThreadRunning", &I3DR::Phase::PhobosStereoCamera::isReadThreadRunning)
+        .def("getReadThreadResult", &I3DR::Phase::PhobosStereoCamera::getReadThreadResult)
+        .def("getWidth", &I3DR::Phase::PhobosStereoCamera::getWidth)
+        .def("getHeight", &I3DR::Phase::PhobosStereoCamera::getHeight)
+        .def("getDownsampleFactor", &I3DR::Phase::PhobosStereoCamera::getDownsampleFactor)
+        .def("enableDataCapture", &I3DR::Phase::PhobosStereoCamera::enableDataCapture)
+        .def("setDataCapturePath", &I3DR::Phase::PhobosStereoCamera::setDataCapturePath)
+        .def("getCaptureCount", &I3DR::Phase::PhobosStereoCamera::getCaptureCount)
+        .def("resetCaptureCount", &I3DR::Phase::PhobosStereoCamera::resetCaptureCount)
+        .def("setLeftFlipX", &I3DR::Phase::PhobosStereoCamera::setLeftFlipX)
+        .def("setLeftFlipY", &I3DR::Phase::PhobosStereoCamera::setLeftFlipY)
+        .def("setRightFlipX", &I3DR::Phase::PhobosStereoCamera::setRightFlipX)
+        .def("setRightFlipY", &I3DR::Phase::PhobosStereoCamera::setRightFlipY)
+        .def("setDownsampleFactor", &I3DR::Phase::PhobosStereoCamera::setDownsampleFactor)
+        .def("disconnect", &I3DR::Phase::PhobosStereoCamera::disconnect);
 
 }
