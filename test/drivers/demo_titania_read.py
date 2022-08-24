b81c8ad94cdf7390d4bd57fcf69ff2f573bbbae3
kinyipchan913 <100573103+kinyipchan913@users.noreply.github.com>
2022-08-24 10:13:30 +0100

    added demo_titania_read

---
+++ test/drivers/demo_titania_read.py
@@ -0,0 +1,125 @@
+import cv2
+import os
+from phase.pyphase.stereocamera import TitaniaStereoCamera
+from phase.pyphase.types import CameraDeviceType, CameraInterfaceType
+from phase.pyphase.types import CameraDeviceInfo, StereoMatcherType
+from phase.pyphase.calib import StereoCameraCalibration
+from phase.pyphase.stereomatcher import StereoI3DRSGM, StereoParams
+from phase.pyphase.stereomatcher import createStereoMatcher
+from phase.pyphase import scaleImage, normaliseDisparity
+from phase.pyphase import disparity2xyz, savePLY
+
+
+# Define camera info
+# Replace camera_name, left_serial & right serial
+# with unique camera serials
+camera_name = "746974616e24317"
+left_serial = "40098272"
+right_serial = "40098282"
+device_type = CameraDeviceType.DEVICE_TYPE_TITANIA
+interface_type = CameraInterfaceType.INTERFACE_TYPE_USB
+
+script_path = os.path.dirname(os.path.realpath(__file__))
+
+# Define calibration file
+# Calibrate through Vision Stereo toolkit
+test_folder = os.path.join(script_path, "..", ".phase_test")
+data_folder = os.path.join(script_path, "..", "data")
+left_yaml = os.path.join(data_folder, "titania_left.yaml")
+right_yaml = os.path.join(data_folder, "titania_right.yaml")
+out_ply = os.path.join(test_folder, "titania_out.ply")
+
+# Parameters for read and display 20 frames
+downsample_factor = 1.0
+display_downsample = 0.25
+capture_count = 20
+exposure_value = 10000
+
+# Check for license
+i3drsgm = StereoI3DRSGM()
+license_valid = i3drsgm.isLicenseValid()
+if license_valid:
+    print("I3DRSGM license accepted")
+else:
+    print("Missing or invalid I3DRSGM license")
+
+    # Check for I3DRSGM license
+if license_valid:
+    stereo_params = StereoParams(
+        StereoMatcherType.STEREO_MATCHER_I3DRSGM,
+        9, 0, 49, True
+    )
+else:
+    stereo_params = StereoParams(
+        StereoMatcherType.STEREO_MATCHER_BM,
+        11, 0, 25, False
+    )
+# Load calibration
+calibration = StereoCameraCalibration.calibrationFromYAML(
+    left_yaml, right_yaml)
+
+# Create stereo matcher
+matcher = createStereoMatcher(stereo_params)
+
+# Define titania info from connection
+device_info = CameraDeviceInfo(
+    left_serial, right_serial, camera_name,
+    device_type,
+    interface_type)
+tinaniaCam = TitaniaStereoCamera(device_info)
+
+
+ret = tinaniaCam.connect()
+if (ret):
+    tinaniaCam.startCapture()
+    tinaniaCam.setExposure(exposure_value)
+    for i in range(0, capture_count):
+        if (not tinaniaCam.isConnected()):
+            break
+        read_result = tinaniaCam.read()
+
+        if read_result.valid:
+            rect_image_pair = calibration.rectify(read_result.left, read_result.right)
+            rect_img_left = rect_image_pair.left
+            rect_img_right = rect_image_pair.right
+
+            match_result = matcher.compute(rect_img_left, rect_img_right)
+
+            if not match_result.valid:
+                print("Failed to process stereo")
+                continue
+
+            disparity = match_result.disparity
+
+            xyz = disparity2xyz(
+                disparity, calibration.getQ())
+
+            if display_downsample != 1.0:
+                img_left = scaleImage(
+                        rect_img_left, display_downsample)
+                img_right = scaleImage(
+                        rect_img_right, display_downsample)
+                img_disp = scaleImage(
+                        normaliseDisparity(
+                            disparity), display_downsample)
+
+            else:
+                img_left = rect_img_left
+                img_right = rect_img_right
+                img_disp = normaliseDisparity(disparity)
+              
+            cv2.imshow("Left", img_left)
+            cv2.imshow("Right", img_right)
+            cv2.imshow("Disparity", img_disp)
+            c = cv2.waitKey(1)
+            if c == ord('p'):
+                save_success = savePLY(out_ply, xyz, rect_img_left)
+                if save_success:
+                    print("Pointcloud saved to " + out_ply)
+            if c == ord('q'):
+                break
+        else:
+            tinaniaCam.disconnect()
+            raise Exception("Failed to read stereo result")
+            
+cv2.destroyAllWindows()
